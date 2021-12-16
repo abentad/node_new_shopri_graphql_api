a1028a10f3102bf1cc481c7dcf1250ac75e4c24e
Abenezer <abentad4@gmail.com>
2021-12-16 15:09:21 +0300

    message and chat fetch and update and create done

--- src/resolvers/Conversation.js
+++ src/resolvers/Conversation.js
@@ -2,7 +2,16 @@ const Conversation = {
     async lastMessageSender(parent, args, { db }, info){
         const [user] = await db.users.findAll({where: { id: parent.lastMessageSenderId }});
         return user;
+    },
+    async sender(parent, args, { db }, info){
+        const [user] = await db.users.findAll({where: { id: parent.senderId }});
+        return user;
+    },
+    async receiver(parent, args, { db }, info){
+        const [user] = await db.users.findAll({where: { id: parent.receiverId }});
+        return user;
     }
+
 };
 
 module.exports = Conversation;
